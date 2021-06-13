0efc1fe7e8a5eed0a9ac058470dde475aaf8c260
danurrizad <79238162+danurrizad@users.noreply.github.com>
2021-06-13 14:57:41 +0700

    Add files via upload

---
+++ SchedulLOG/additional/script/script.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('#cbPass').click(function () {
+        if ($(this).is(':checked')) {
+            $('#tbPass').atrr('type', 'text');
+            $('#tbPassConf').atrr('type', 'text');
+        }
+        else {
+            $('#tbPass').atrr('type', 'password');
+            $('#tbPassConf').atrr('type', 'password');
+        }
+    })
+})
